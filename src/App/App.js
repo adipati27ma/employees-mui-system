d7c4d05bdb20c57db434bd5610f7fe8a5176679f
adipati27ma <adipati.cs.samsung@gmail.com>
2020-12-24 23:51:06 +0700

    initial commit & makeStyles JSS

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import SideMenu from "../components/SideMenu";
+
+function App() {
+  return (
+    // <></> itu sama dengan <React.Fragment></React.Fragment>
+    <>
+      <SideMenu />
+      <div>Here we go</div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/SideMenu.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { makeStyles, withStyles } from "@material-ui/core";
+
+// withStyles & makeStyles
+const useStyles = makeStyles({
+  sideMenu: {
+    display: "flex",
+    flexDirection: "column",
+    position: "absolute",
+    left: "0px",
+    width: "320px",
+    height: "100%",
+    backgroundColor: "#253053",
+  },
+});
+
+export default function SideMenu() {
+  const classes = useStyles();
+
+  return <div className={classes.sideMenu}></div>;
+}
