f358a4b7a6a9ceb080b04b4f458e593146dd0a76
adipati27ma <adipati.cs.samsung@gmail.com>
2020-12-24 23:56:01 +0700

    using withStyles JSS in SideMenu

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,8 +1,27 @@
 import React from "react";
 import { makeStyles, withStyles } from "@material-ui/core";
 
-// withStyles & makeStyles
-const useStyles = makeStyles({
+// using makeStyles ----------------
+// const useStyles = makeStyles({
+//   sideMenu: {
+//     display: "flex",
+//     flexDirection: "column",
+//     position: "absolute",
+//     left: "0px",
+//     width: "320px",
+//     height: "100%",
+//     backgroundColor: "#253053",
+//   },
+// });
+
+// export default function SideMenu() {
+//   const classes = useStyles();
+
+//   return <div className={classes.sideMenu}></div>;
+// }
+
+// using withStyles
+const style = {
   sideMenu: {
     display: "flex",
     flexDirection: "column",
@@ -12,10 +31,12 @@ const useStyles = makeStyles({
     height: "100%",
     backgroundColor: "#253053",
   },
-});
+};
 
-export default function SideMenu() {
-  const classes = useStyles();
+const SideMenu = (props) => {
+  const { classes } = props;
 
   return <div className={classes.sideMenu}></div>;
-}
+};
+
+export default withStyles(style)(SideMenu);
